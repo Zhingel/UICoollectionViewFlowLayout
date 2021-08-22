adf5ec6ad9d51425aecf6af42fc777d00ee336dc
Stas <staszhingel@icloud.com>
2021-08-22 09:22:12 +0300

    header and footer

--- UICoollectionViewFlowLayout/CollectionViewCell.swift
+++ UICoollectionViewFlowLayout/CollectionViewCell.swift
@@ -7,6 +7,52 @@
 
 import UIKit
 
+class MyHeader: UICollectionViewCell {
+    weak var textLabel: UILabel!
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(label)
+        NSLayoutConstraint.activate ([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
+        textLabel = label
+        contentView.backgroundColor = .blue
+        textLabel.textAlignment = .center
+        textLabel.text = "Header"
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+class MyFooter: UICollectionViewCell {
+    weak var textLabel: UILabel!
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(label)
+        NSLayoutConstraint.activate ([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
+        textLabel = label
+        contentView.backgroundColor = .blue
+        textLabel.textAlignment = .center
+        textLabel.text = "Footer"
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 class CollectionViewCell: UICollectionViewCell {
     weak var textLabel: UILabel!
     override init(frame: CGRect) {
--- UICoollectionViewFlowLayout/ViewController.swift
+++ UICoollectionViewFlowLayout/ViewController.swift
@@ -32,8 +32,9 @@ class ViewController: UIViewController {
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: "MyCell")
-    
-    
+        collectionView.register(MyHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "Header")
+        collectionView.register(MyFooter.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: "Footer")
+        
 }
 }
 
@@ -45,8 +46,41 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath) as! CollectionViewCell
         cell.textLabel.text = String(indexPath.row)
+        cell.layer.cornerRadius = 20
+        cell.layer.masksToBounds = true
         return cell
     }
-    
-    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            let headerCell = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "Header", for: indexPath) as! MyHeader
+            return headerCell
+        case UICollectionView.elementKindSectionFooter:
+            let footerCell = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "Footer", for: indexPath) as! MyFooter
+            return footerCell
+        default:
+            assert(false)
+        }
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        CGSize(width: 0, height: 80)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        CGSize(width: 0, height: 80)
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+       
+       
+        return CGSize(width: collectionView.bounds.size.width - 16 , height: 120)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        20
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    }
+
 }
